857be086c1ccf90312f27ce92dd44a348e0aa2f6
Patrick Sheng <sheng.patrick@gmail.com>
2025-10-08 16:39:05 +1300

    feat: initialised sdm project with docker, and created schema migration plan

---
+++ schema/user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
